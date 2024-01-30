b46203e49f9c8e42f6339737f5906be7a70184ad
KARAN NATH <112185032+Karan60@users.noreply.github.com>
2024-01-30 18:49:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var Btn = document.getElementById("Btn");
+var light = document.getElementById("light");
+function toggleBtn(){
+    Btn.classList.toggle("active");
+    light.classList.toggle("on");
+}
